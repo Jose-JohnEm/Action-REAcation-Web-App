a52570f8ebe0432f8d1f0a26bd9810f8ca0084c1
Lucas Guichard <lucas.guichard@epitech.eu>
2022-01-21 15:47:35 +0100

    🎉 Init client web project

--- client_web/src/App.test.tsx
+++ client_web/src/App.test.tsx
@@ -7,17 +7,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-
-describe('Test pour tester les tests de react', () => {
-  let petite_val = 0
-  let grande_val = 1
-
-  test('Test de supériorité', () => {
-      expect(grande_val).toBeGreaterThan(petite_val)
-  })
-  
-  test("Test d'infériorité", () => {
-      expect(petite_val).toBeLessThan(grande_val)
-  })
-});
