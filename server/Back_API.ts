022cbe98715e9ae816a56e1f1165ec34f94f6df4
Jose Jonathan Emmanuel <jonathan-emmanuel.jose@epitech.eu>
2022-01-28 19:21:32 +0100

    get hosts ip

--- server/Back_API.ts
+++ server/Back_API.ts
@@ -1,9 +1,27 @@
 import express from 'express'
+import { networkInterfaces } from 'os'
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/about.json', (req,res) => {
+  const nets = networkInterfaces();
+  const results : object = {};
+
+  for (const name of Object.keys(nets)) {
+      for (const net of nets[name]) {
+          if (net.family === 'IPv4' && !net.internal) {
+              if (!results[name]) {
+                  results[name] = [];
+              }
+              results[name].push(net.address);
+          }
+      }
+  }
+  res.send({client: results['eth0'][0]})
 })
 
 app.listen(port, () => {
--- server/dist/Back_API.js
+++ server/dist/Back_API.js
@@ -4,10 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const os_1 = require("os");
 const app = (0, express_1.default)();
 const port = 8080;
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+app.get('/about.json', (req, res) => {
+    const nets = (0, os_1.networkInterfaces)();
+    const results = {};
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                results[name].push(net.address);
+            }
+        }
+    }
+    res.send({ client: results['eth0'][0] });
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
