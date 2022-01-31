8cd3b2fa8db9380cf49b4e18cf6fc184471a4f26
Mehdi <mehdi.zehri@epitech.eu>
2022-01-31 17:35:50 +0100

    ✨ Added Redux, React Navigation, React Paper

---
+++ mobile_front/sources/reducers/Actions/Auth.js
@@ -0,0 +1,24 @@
+export const SET_USER_LOGGED_IN = 'SET_USER_LOGGED_IN';
+export const SET_USER_LOGGED_OUT = 'SET_USER_LOGGED_OUT';
+
+export const setUserLoggedIn = () => {
+  return dispatch => {
+    dispatch({
+      type: SET_USER_LOGGED_IN,
+      payload: true,
+    });
+  };
+};
+
+export const setUserLoggedOut = () => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: SET_USER_LOGGED_OUT,
+        payload: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
