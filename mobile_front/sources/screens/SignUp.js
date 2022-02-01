6459f8ec9bccdc38415b8e0056ee7027af4eb624
Mehdi <mehdi.zehri@epitech.eu>
2022-02-01 21:06:30 +0100

    ✨ Added SignUp.

--- mobile_front/sources/screens/SignUp.js
+++ mobile_front/sources/screens/SignUp.js
@@ -1,46 +1,81 @@
 import React from 'react';
-import { View, StyleSheet} from 'react-native';
-import { Button, Caption, TextInput } from 'react-native-paper';
+import { View, StyleSheet, Text} from 'react-native';
+import { Caption, TextInput, Title } from 'react-native-paper';
+import LargeButton from '../components/LargeButton';
+import PropTypes from 'prop-types';
+import SocialButtonGroup from '../components/SocialButtonGroup';
+import LogoContainer from '../components/LogoContainer';
 
-const SignIn = () => {
+const SignUpForm = () => {
+  const [fullName, setFullName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPasswd] = React.useState('');
 
   return (
+    <View>
+      <TextInput
+        placeholder="Full Name"
+        value={fullName}
+        mode="outlined"
+        onChangeText={text => setFullName(text)}
+        style={{marginBottom: 10}}
+      />
+      <TextInput
+        placeholder="Email"
+        value={email}
+        mode="outlined"
+        onChangeText={text => setEmail(text)}
+        style={{marginBottom: 10}}
+      />
+      <TextInput
+        placeholder="Password"
+        mode="outlined"
+        value={password}
+        secureTextEntry={true}
+        onChangeText={text => setPasswd(text)}
+        style={{marginBottom: 10}}
+      />
+      <LargeButton onPress={() => console.log('Sign Up')} mode="contained">
+          Sign Up
+      </LargeButton>
+    </View>
+  );
+};
+
+const SignUp = ({navigation}) => {
+  return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          mode="outlined"
-          onChangeText={text => setEmail(text)}
-        />
+      <LogoContainer />
+      <View style={{flex: 1}}>
+        <Title>
+          Create New Account
+        </Title>
+        <Caption>
+          Please fill in the form to continue
+        </Caption>
+        <SignUpForm />
+        <SocialButtonGroup />
+        <Caption style={{textAlign: 'center'}}>
+          {'Already have an account ? '}
+          <Text onPress={() => navigation.push('Sign In')} style={{color: '#0077b6'}}>
+            Sign In
+          </Text>
+        </Caption>
       </View>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Password"
-          mode="outlined"
-          value={password}
-          onChangeText={text => setPasswd(text)}
-        />
-      </View>
-      <View>
-        <Caption>Forgot password ?</Caption>
-      </View>
-      <Button icon="login-variant" mode="contained" onPress={() => console.log('Pressed')}>
-        Sign In
-      </Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     padding: 30,
   },
-  inputContainer : {
-    paddingBottom: 10,
-  }
 });
 
-export default SignIn;
+SignUp.propTypes = {
+  navigation: PropTypes.object,
+};
+
+
+export default SignUp;
