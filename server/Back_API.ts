5463a7e74eb19a29e59b8eccd7afce6b5dfe42cc
Jose Jonathan Emmanuel <jonathan-emmanuel.jose@epitech.eu>
2022-02-01 11:06:35 +0100

    move about.json code

--- server/Back_API.ts
+++ server/Back_API.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { networkInterfaces } from 'os'
+import about_json from './src/about_json'
 const app = express()
 const port = 8080
 
@@ -7,22 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/about.json', (req,res) => {
-  const nets = networkInterfaces();
-  const results : object = {};
-
-  for (const name of Object.keys(nets)) {
-      for (const net of nets[name]) {
-          if (net.family === 'IPv4' && !net.internal) {
-              if (!results[name]) {
-                  results[name] = [];
-              }
-              results[name].push(net.address);
-          }
-      }
-  }
-  res.send({client: results['eth0'][0]})
-})
+app.get('/about.json', about_json)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
