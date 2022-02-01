10d9f60fc7b68c53eb8d8d8ec55c0eba6038d4f5
Jose Jonathan Emmanuel <jonathan-emmanuel.jose@epitech.eu>
2022-02-01 11:49:22 +0100

    Connect to MongoDB Collection

--- server/Back_API.ts
+++ server/Back_API.ts
@@ -1,15 +1,26 @@
 import express from 'express'
-import { networkInterfaces } from 'os'
 import about_json from './src/about_json'
-const app = express()
-const port = 8080
+import mongoose from 'mongoose'
 
+///// Connect MongoDB and Server /////
+const dbURI = 'mongodb+srv://area_ish-ish_2022:YeO7XT8eOtbQFK9H@cluster0.4jz3r.mongodb.net/area2022?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+  .then((result) => app.listen(port, successServerStarted))
+  .catch((error) => console.log(error));
+
+const app = express()
+const port = (parseInt(process.argv[2]) < 65536) ? parseInt(process.argv[2]) : 8080
+
+const successServerStarted = () => {
+  console.log(`MongoDB Connected succesfully !\nExample app listening at http://localhost:${port}`)
+}
+
+
+///// Routes /////
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/about.json', about_json)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
