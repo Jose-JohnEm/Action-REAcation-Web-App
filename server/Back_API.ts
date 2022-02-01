9912e57b18da7e38b00dd343f31cafcc51c455d7
Jose Jonathan Emmanuel <jonathan-emmanuel.jose@epitech.eu>
2022-02-01 18:22:33 +0100

    Registration almost done

--- server/Back_API.ts
+++ server/Back_API.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import about_json from './src/about_json'
 import mongoose from 'mongoose'
 
+import User from './models/users'
+
+import authenticator from './src/auth/authenticator'
+
 ///// Connect MongoDB and Server /////
 const dbURI = 'mongodb+srv://area_ish-ish_2022:YeO7XT8eOtbQFK9H@cluster0.4jz3r.mongodb.net/area2022?retryWrites=true&w=majority';
 mongoose.connect(dbURI)
-  .then((result) => app.listen(port, successServerStarted))
-  .catch((error) => console.log(error));
+.then((result) => app.listen(port, successServerStarted))
+.catch((error) => console.log(error));
 
 const app = express()
 const port = (parseInt(process.argv[2]) < 65536) ? parseInt(process.argv[2]) : 8080
@@ -16,11 +20,16 @@ const successServerStarted = () => {
 }
 
 
-///// Routes /////
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+///// Add custom debug middleware /////
+
+app.use((req, res, next) => {
+  console.log(req.url)
+  next()
 })
 
+
+///// Routes /////
+
+app.get('/', about_json)
 app.get('/about.json', about_json)
-
-
+app.use('/auth', authenticator)
---
+++ server/src/auth/authenticator.ts
@@ -0,0 +1,33 @@
+import UserData from '../../models/users'
+
+import registerAccount from './register'
+
+import express from 'express'
+const router = express.Router()
+
+
+const editAccount = (req, res) => {
+
+}
+
+const connectAccount = (req, res) => {
+
+}
+
+const deleteAccount = (req, res) => {
+
+}
+
+const invalidMethod = (req, res) => {
+  res.status(404).json({'error': 'Invalid Method'})
+}
+
+router.route('/')
+  .get(connectAccount)
+  .post(registerAccount)
+  .put(editAccount)
+  .delete(deleteAccount)
+  .all(invalidMethod)
+
+
+export default router
