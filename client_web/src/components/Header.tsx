0667b31df47c558ad544e49456e6240862c6b811
Lucas Guichard <lucas.guichard@epitech.eu>
2022-02-03 15:23:39 +0100

    ✨ Added flexible header

--- client_web/src/components/Header.tsx
+++ client_web/src/components/Header.tsx
@@ -1,12 +1,15 @@
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import COLORS from '../constants/colors';
 
 const Header = () => {
     return (
-      <AppBar position='static' sx={{ bgcolor: COLORS.WHITE }}>
-        <Toolbar>
-            <Button variant='text'sx={{ paddingLeft: 3, paddingRight: 3 }}>Sign up</Button>
-            <Button variant='contained' sx={{ bgcolor: COLORS.DARKGRAY, borderRadius: 2, paddingLeft: 3, paddingRight: 3 }}>Log in</Button>
+      <AppBar elevation={0} sx={{ bgcolor: COLORS.WHITE }}>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+            <Button><img src='logo.png' alt='AREA logo' width='20%' /></Button>
+            <Box>
+              <Button variant='text'sx={{ paddingLeft: 3, paddingRight: 3, mr: 2 }}>Sign up</Button>
+              <Button variant='contained' sx={{ bgcolor: COLORS.DARKGRAY, borderRadius: 2, paddingLeft: 3, paddingRight: 3 }}>Log in</Button>
+            </Box>
         </Toolbar>
       </AppBar>
     )
