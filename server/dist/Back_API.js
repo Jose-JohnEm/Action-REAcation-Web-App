5ff8c0030296a725c406c405123c3c7f2e484a1d
KinoFool <nicolas.mary@epitech.eu>
2022-02-07 23:14:30 +0100

    FIX: Upgrade mongoose version to 6.2.0 now work with typescript correctly

--- server/dist/Back_API.js
+++ server/dist/Back_API.js
@@ -5,16 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const about_json_1 = __importDefault(require("./src/about_json"));
-// Connect MongoDB
-const dbURI = 'mongodb+srv://area_ish-ish_2022:YeO7XT8eOtbQFK9H@cluster0.4jz3r.mongodb.net/area2022?retryWrites=true&w=majority';
-//mongoose.connect(dbURI);
+const mongoose_1 = __importDefault(require("mongoose"));
+const authenticator_1 = __importDefault(require("./src/auth/authenticator"));
 const app = (0, express_1.default)();
-const port = parseInt(process.argv[1]) < 10000 ? parseInt(process.argv[1]) : 8080;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const port = (parseInt(process.argv[2]) < 65536) ? parseInt(process.argv[2]) : 8080;
+///// Connect MongoDB and Server /////
+const successServerStarted = () => {
+    console.log(`MongoDB Connected succesfully !\nExample app listening at http://localhost:${port}`);
+};
+const dbURI = 'mongodb+srv://area_ish-ish_2022:YeO7XT8eOtbQFK9H@cluster0.4jz3r.mongodb.net/area2022?retryWrites=true&w=majority';
+mongoose_1.default.connect(dbURI)
+    .then((result) => app.listen(port, successServerStarted))
+    .catch((error) => console.log(error));
+///// Add custom debug middleware /////
+app.use((req, res, next) => {
+    console.log(req.url);
+    next();
 });
+///// Routes /////
+app.get('/', about_json_1.default);
 app.get('/about.json', about_json_1.default);
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use('/auth', authenticator_1.default);
+exports.default = app;
 //# sourceMappingURL=Back_API.js.map
