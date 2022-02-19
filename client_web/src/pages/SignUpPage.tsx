ed2e6a1947438ab86f982c8063f546154dc3db10
Lucas Guichard <lucas.guichard@epitech.eu>
2022-02-19 16:50:56 +0100

    ✨ Added required checkbox on signup form

--- client_web/src/pages/SignUpPage.tsx
+++ client_web/src/pages/SignUpPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Grid, Typography, Box, TextField, Button, InputAdornment, IconButton } from '@mui/material';
+import { Grid, Typography, Box, TextField, Button, InputAdornment, IconButton, FormControlLabel, Checkbox } from '@mui/material';
 import COLORS from '../constants/colors';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
@@ -21,8 +21,8 @@ const SignUpForm = () => {
   };
 
   return (
-    <Box component='form' onSubmit={handleSubmit}>
-      <TextField margin='normal' required fullWidth label='Last Name' name='last_name' sx={{ mt: 5 }} helperText='Only alphabetic characters and spaces/dashes' inputProps={{ pattern: '^[a-zA-Z -]+$' }}/>
+    <Box component='form' onSubmit={handleSubmit} sx={{ ml: 20, mr: 20 }}>
+      <TextField margin='normal' required fullWidth label='Last Name' name='last_name' sx={{ mt: 10 }} helperText='Only alphabetic characters and spaces/dashes' inputProps={{ pattern: '^[a-zA-Z -]+$' }}/>
       <TextField margin='normal' required fullWidth label='First Name' name='first_name' helperText='Only alphabetic characters and spaces/dashes' inputProps={{ pattern: '^[a-zA-Z -]+$' }}/>
       <TextField margin='normal' required fullWidth label='Email' name='email' inputProps={{ pattern: '^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[A-Za-z]+$' }}/>
       <TextField margin='normal' required fullWidth type={showPassword ? 'text' : 'password'} label='Password' name='password' sx={{ mb: 5 }} helperText='Only alphanumeric characters. Minimum 8 characters' inputProps={{ pattern: '[a-zA-Z0-9]{8,}' }}
@@ -36,6 +36,11 @@ const SignUpForm = () => {
           </InputAdornment>)
         }}
       />
+      <FormControlLabel sx={{ mt: -3, mb: 3 }} control={<Checkbox required />} label={
+        <Typography textAlign='left' fontSize={12} color='text.secondary' sx={{ fontStyle: 'oblique' }}>
+          By checking this box, you agree to our Terms. Learn how we collect, use and share your data in our Data Policy and how we use cookies and similar technology in our Cookie Policy.
+        </Typography>
+      }/>
       <Button type='submit' fullWidth variant='contained' sx={{ bgcolor: COLORS.DARKGRAY }}>
         Create
       </Button>
@@ -58,5 +63,4 @@ const SignUpPage = () => {
 
 export default SignUpPage;
 
-// TODO: mandatory checkbox
 // TODO: upload avatar
