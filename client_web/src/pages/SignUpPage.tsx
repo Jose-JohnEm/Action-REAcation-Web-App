13ce0d380b3f9983ba4ab8b59975391a7067a902
Lucas Guichard <lucas.guichard@epitech.eu>
2022-02-19 16:15:07 +0100

    ✨ Added visibility option on password text field

--- client_web/src/pages/SignUpPage.tsx
+++ client_web/src/pages/SignUpPage.tsx
@@ -1,30 +1,46 @@
-import { Grid, Typography, Box, TextField, Button } from '@mui/material';
+import { useState } from 'react';
+import { Grid, Typography, Box, TextField, Button, InputAdornment, IconButton } from '@mui/material';
 import COLORS from '../constants/colors';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const SignUpForm = () => {
-    const handleSubmit = (event: React.ChangeEvent<any>) => {
-      event.preventDefault();
-      const data = new FormData(event.currentTarget);
-      const body = {
-        last_name: data.get('last_name'),
-        first_name: data.get('first_name'),
-        email: data.get('email'),
-        password: data.get('password')
-      };
-      // console.log(body);
-    };
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
 
-    return (
-      <Box component='form' onSubmit={handleSubmit}>
-        <TextField margin='normal' required fullWidth label='Last Name' name='last_name' sx={{ mt: 5 }}/>
-        <TextField margin='normal' required fullWidth label='First Name' name='first_name'/>
-        <TextField margin='normal' required fullWidth label='Email' name='email'/>
-        <TextField margin='normal' required fullWidth type='password' label='Password' name='password' sx={{ mb: 5 }}/>
-        <Button type='submit' fullWidth variant='contained' sx={{ bgcolor: COLORS.DARKGRAY }}>
-          Create
-        </Button>
-      </Box>
-    );
+  const handleSubmit = (event: React.ChangeEvent<any>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const body = {
+      last_name: data.get('last_name'),
+      first_name: data.get('first_name'),
+      email: data.get('email'),
+      password: data.get('password')
+    };
+    // console.log(body);
+  };
+
+  return (
+    <Box component='form' onSubmit={handleSubmit}>
+      <TextField margin='normal' required fullWidth label='Last Name' name='last_name' sx={{ mt: 5 }} helperText='Only alphabetic characters and spaces/dashes' inputProps={{ pattern: '^[a-zA-Z -]+$' }}/>
+      <TextField margin='normal' required fullWidth label='First Name' name='first_name' helperText='Only alphabetic characters and spaces/dashes' inputProps={{ pattern: '^[a-zA-Z -]+$' }}/>
+      <TextField margin='normal' required fullWidth label='Email' name='email' inputProps={{ pattern: '^[a-zA-Z0-9]+@[a-zA-Z0-9]+.[A-Za-z]+$' }}/>
+      <TextField margin='normal' required fullWidth type={showPassword ? 'text' : 'password'} label='Password' name='password' sx={{ mb: 5 }} helperText='Only alphanumeric characters. Minimum 8 characters' inputProps={{ pattern: '[a-zA-Z0-9]{8,}' }}
+        InputProps={{ endAdornment: (
+          <InputAdornment position='end'>
+            <IconButton
+              onClick={handleClickShowPassword}
+            >
+              {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            </IconButton>
+          </InputAdornment>)
+        }}
+      />
+      <Button type='submit' fullWidth variant='contained' sx={{ bgcolor: COLORS.DARKGRAY }}>
+        Create
+      </Button>
+    </Box>
+  );
 };
 
 const SignUpPage = () => {
@@ -42,8 +58,5 @@ const SignUpPage = () => {
 
 export default SignUpPage;
 
-
-// TODO: regex
-// TODO: eye password
 // TODO: mandatory checkbox
 // TODO: upload avatar
