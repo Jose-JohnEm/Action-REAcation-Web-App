f888a204336eccd06b44d8c2c45db0f4806d187b
KinoFool <nicolas.mary@epitech.eu>
2022-02-21 15:04:16 +0100

    WIP: github webhooks

--- server/src/event/action/Github/github.ts
+++ server/src/event/action/Github/github.ts
@@ -1,17 +1,17 @@
 // When receiving a push event from GitHub, we want to update the
 // corresponding repository's status.
 
-var gh_push = function (headers, body) {
+const gh_push = function (headers, body) {
     // console.log(headers);
     // console.log(body);
-}
+};
 
-var gh_pull_request = function (headers, body) {
+const gh_pull_request = function (headers, body) {
     // console.log(headers);
     // console.log(body);
-}
+};
 
-var gh_star = function (body) {
+const gh_star = function (body) {
     if (body.action == "created") {
         console.log(body);
         console.log("The user " + body.sender.login + " starred the repo " + body.repository.full_name);
@@ -20,6 +20,15 @@ var gh_star = function (body) {
         console.log("The user " + body.sender.login + " unstarred the repo " + body.repository.full_name + " :(");
     }
 
+};
+
+const gh_commit = function (body) {
+
 }
 
-export {gh_push, gh_pull_request, gh_star}
+
+const gh_fork = function (body) {
+
+}
+
+export {gh_push, gh_pull_request, gh_star, gh_commit, gh_fork};
--- server/src/webhooks/webhooks.ts
+++ server/src/webhooks/webhooks.ts
@@ -1,44 +1,43 @@
 import express from 'express'
-
-import {gh_pull_request, gh_push, gh_star} from "../event/action/Github/github";
+import {gh_commit, gh_fork, gh_pull_request, gh_push, gh_star} from "../event/action/Github/github";
 
 const router = express.Router()
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded());
-router.route('/github')
-    .post((req, res) => {
-        if (!req.headers['x-github-event']) {
-            console.error('Not a GitHub event !');
-            res.status(400).send('Webhook Error: Invalid event type')
-            return;
-        }
+router.route('/github').post((req, res) => {
+    if (!req.headers['x-github-event']) {
+        console.error('Not a GitHub event !');
+        res.status(400).send('Webhook Error: Invalid event type')
+        return;
+    }
 
-        const body = JSON.parse(req.body.payload);
-        console.log("GitHub event received: " + req.headers['x-github-event'] + " - " + body.action);
+    const body = JSON.parse(req.body.payload);
+    const event = req.headers['x-github-event'].toString();
 
-        const sender = body.sender.login;
-        // TODO; Verify if the sender is in our database
+    console.log("GitHub event received: " + event + " - " + body.action);
+
+    const sender = body.sender.login;
+    // TODO; Verify if the sender is in our database
+
+    res.status(200).send('Webhook received')
 
 
-        res.status(200).send('Webhook received')
+    // Create list of event and call the corresponding function
+    let events = {
+        'pull_request': gh_pull_request,
+        'push': gh_push,
+        'star': gh_star,
+        'fork': gh_fork,
+        'commit': gh_commit,
+    }
 
-        // Get the event type and call the associated function
-        switch (req.headers['x-github-event']) {
-            case 'push':
-                gh_push(req.headers, body);
-                break;
-            case 'pull_request':
-                gh_pull_request(req.headers, body);
-                break;
-            case 'star':
-                gh_star(body);
-                break;
-            default:
-                console.error('Unsupported event type: ' + req.headers['x-github-event']);
-                break;
-        }
-    })
+    if (!events[event]) {
+        console.error('Github, Unsupported event type!');
+        return;
+    }
+    events[event](body);
+});
 
 
 export default router
