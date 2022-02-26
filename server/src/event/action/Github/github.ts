f111af47c3bf0c4deee301058e6650b3188b6958
KinoFool <nicolas.mary@epitech.eu>
2022-02-26 17:57:04 +0100

    Github pull request open and close

--- server/src/event/action/Github/github.ts
+++ server/src/event/action/Github/github.ts
@@ -5,6 +5,9 @@ import {getUserFromGithubAction, getUserReaction} from "../../../db/event";
 import handleReactions from "../../reaction/msg_sender";
 
 const gh_push = async function (body, sender, repo) {
+    if (body.commits.length == 0)
+        return;
+
     const users = await getUserFromGithubAction("new_push", repo)
     if (!users) {
         console.log("No user found for this push event: " + repo);
@@ -12,7 +15,6 @@ const gh_push = async function (body, sender, repo) {
     }
 
     const {pusher, repository, commits} = body;
-
     const message = pusher.name + " push " + commits.length + " commit(s) on " + repo + "\nHead commit is \"" + body.head_commit.message + "\""
 
     let reaction;
@@ -23,7 +25,37 @@ const gh_push = async function (body, sender, repo) {
 };
 
 const gh_pull_request = async function (body, sender, repo) {
-    // const user = await getUserFromGithubAction("new_push", sender, repo);
+    const state = body.action;
+    console.log(JSON.parse(JSON.stringify(body)))
+
+    let action;
+    if (state == "open")
+        action = "new_pull_request";
+    else if (state == "closed")
+        action = "closed_pull_request";
+    else
+        return;
+
+    const users = await getUserFromGithubAction(action, repo)
+    if (!users) {
+        console.log("No user found for this pull_request event: " + repo);
+        return;
+    }
+
+    let message;
+    if (state == "open") {
+        const {pull_request} = body;
+        message = pull_request.user.login + " opened a pull request on " + repo + "\nTitle: \"" + pull_request.title + "\""
+    } else if (state == "closed") {
+        const {pull_request} = body;
+        message = pull_request.user.login + " closed a pull request on " + repo + "\nTitle: \"" + pull_request.title + "\""
+    }
+
+    let reaction;
+    users.forEach(user => {
+        reaction = getUserReaction(user, "github", action, {"repository": repo})
+        handleReactions(reaction.service, reaction.name, {"message": message})
+    });
 };
 
 const gh_star = async function (body, sender, repo) {
